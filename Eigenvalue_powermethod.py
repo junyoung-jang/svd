94deb686e8b33a0f4a05bb8376c628cc46e5b364
BuildTools <unconfigured@null.spigotmc.org>
2026-05-26 22:04:43 +0900

    5th

--- Eigenvalue_powermethod.py
+++ Eigenvalue_powermethod.py
@@ -20,6 +20,21 @@ def hotelingDeflation(matrix, eigenvalue, eigenvector):
     return powerMethod(after_deflation, 100)  
 # Call powerMethod on the deflated matrix
 
+def iterationhotelingDeflation(matrix, iterations):
+    eigenvalues = []
+    eigenvectors = []
+    eigenvalue, eigenvector = powerMethod(matrix, 100)
+    print("Eigenvalue:", eigenvalue)
+    print("Eigenvector:", eigenvector)
+
+    for i in range(1, iterations):  
+        eigenvalue, eigenvector = hotelingDeflation(matrix, eigenvalue, eigenvector)
+        print(f"Eigenvalue {i}:", eigenvalue)
+        print(f"Eigenvector {i}:", eigenvector)
+        eigenvalues.append(eigenvalue)
+        eigenvectors.append(eigenvector)
+
+    return eigenvalues, eigenvectors
 
 
 
