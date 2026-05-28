e6d8be7ce63210ec46dfe5aeb3d7ede7ce7e0193
BuildTools <unconfigured@null.spigotmc.org>
2026-05-28 23:06:00 +0900

    6th

--- Eigenvalue_powermethod.py
+++ Eigenvalue_powermethod.py
@@ -3,11 +3,13 @@
 import numpy as np
 from matrix_product import matrixProduct
 def powerMethod(matrix, iterations):
-    a = np.ones(matrix.shape[0])
+    a = np.random.rand(matrix.shape[0], 1)
     for i in range(iterations):
         a = matrixProduct(matrix, a)
-        eigenvector = a / np.linalg.norm(a)
-        eigenvalue = np.dot(eigenvector, matrixProduct(matrix, eigenvector)) / np.dot(eigenvector, eigenvector)
+        a = a / np.linalg.norm(a)
+        
+    eigenvector = a
+    eigenvalue = np.dot(eigenvector.T, np.dot(matrix, eigenvector))[0, 0]
     return eigenvalue, eigenvector
 
 
@@ -15,24 +17,22 @@ def powerMethod(matrix, iterations):
 # deflection method have 2 methods but to represent svd using Hotelling's deflation method is efficient
 
 
-def hotelingDeflation(matrix, eigenvalue, eigenvector):
-    after_deflation = matrix - eigenvalue * np.outer(eigenvector, eigenvector)
-    return powerMethod(after_deflation, 100)  
+
 # Call powerMethod on the deflated matrix
 
-def iterationhotelingDeflation(matrix, iterations):
+def hotellingDeflation(matrix, iterations):
     eigenvalues = []
     eigenvectors = []
-    eigenvalue, eigenvector = powerMethod(matrix, 100)
-    print("Eigenvalue:", eigenvalue)
-    print("Eigenvector:", eigenvector)
 
-    for i in range(1, iterations):  
-        eigenvalue, eigenvector = hotelingDeflation(matrix, eigenvalue, eigenvector)
-        print(f"Eigenvalue {i}:", eigenvalue)
-        print(f"Eigenvector {i}:", eigenvector)
+    for i in range(0, iterations):  
+        eigenvalue, eigenvector = powerMethod(matrix, 100)
         eigenvalues.append(eigenvalue)
         eigenvectors.append(eigenvector)
+        print(f"Eigenvalue {i}:", eigenvalue)
+        print(f"Eigenvector {i}:", eigenvector)
+        after_deflation = matrix - eigenvalue * np.outer(eigenvector, eigenvector)
+        eigenvalue, eigenvector = powerMethod(after_deflation, 100)
+        matrix = after_deflation
 
     return eigenvalues, eigenvectors
 
