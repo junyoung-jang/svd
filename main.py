378206fac01fb8cff3224f2780fa67268e6383c5
BuildTools <j.junyoung0000@gmail.com>
2026-06-09 23:37:55 +0900

    test is done with arbitary matrix

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import numpy as np
 from matrix_product import matrixProduct
 import Eigenvalue_powermethod as ep
+from PIL import Image
 
-a = np.arange(0,12, 1)
-a.shape = (3, 4)
+# below is the test code for the power method and deflation method, you can uncomment it to run the test.
+'''
+a = np.array([[1,4,7, 10], [2,8,5, 13]])
 at = a.T
+
 
 ### matrix product
 
@@ -21,7 +24,7 @@ print(testvector)
 
 
 ## Test the deflation method
-eigenvalues, eigenvectors = ep.hotellingDeflation(aat, 3)
+eigenvalues, eigenvectors = ep.hotellingDeflation(aat, 2)
 print("Deflation method eigenvalues:", eigenvalues)
 print("Deflation method eigenvectors:", eigenvectors)   
 
@@ -30,6 +33,10 @@ print("Deflation method eigenvectors:", eigenvectors)
 eigenvalues, eigenvectors = np.linalg.eig(aat)
 print("Numpy's eigenvalues:", eigenvalues)
 print("Numpy's eigenvectors:", eigenvectors)   
+'''
+
+# Let reconstruct arbitrary image using SVD and power method
+
 
 
 
